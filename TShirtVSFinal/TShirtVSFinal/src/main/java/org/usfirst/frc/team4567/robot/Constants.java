b4f7b323c182c4196643628af0d41a0c96aa1fe5
jabbate19 <josephabbateny@gmail.com>
2019-01-05 15:28:47 -0500

    New VSFolder, Added Constants to fix later

---
+++ TShirtVSFinal/TShirtVSFinal/src/main/java/org/usfirst/frc/team4567/robot/Constants.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team4567.robot;
+
+public class Constants{
+    public static final int LeftP=0;
+    public static final int RightP=1;
+    public static final int horn=2;
+    public static final int LeftC=3;
+    public static final int RightC=4;
+
+}
